3720b404b4def92efafab25285908c6866b3ee5a
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-08-29 23:11:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,3 +12,11 @@ function nowServing(katzDeliLine) {
     return 'There is nobody waiting to be served.'
   }
 }
+
+function currentLine(line) {
+  if (line.length > 0) {
+    for (var i = 0; i < line.length; i++)
+  } else {
+    return 'The line is currently empty.'
+  }
+}
