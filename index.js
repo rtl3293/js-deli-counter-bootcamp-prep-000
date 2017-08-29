4b381ab6f90dece9a53ba5c36cfdac19d5dbb43d
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-08-29 23:18:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,16 @@ function nowServing(katzDeliLine) {
 
 function currentLine(line) {
   if (line.length > 0) {
-    for (var i = 0; i < line.length; i++)
+    for (var i = 0; i < line.length; i++) {
+      var name = line[i]
+      var spot = i + 1
+      var order = ``
+      if (i < line.length - 1) {
+        order = order + `${spot}. ${name}, `
+      } else if (i === line.length - 1) {
+        order = order + `${spot}. ${name}`
+      }
+    }
   } else {
     return 'The line is currently empty.'
   }
